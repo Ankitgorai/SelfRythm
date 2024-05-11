28ce2c6f562a9f2c70f574a232e8f832f4d09097
Ankit <95871316+Ankitgorai@users.noreply.github.com>
2024-05-11 14:38:14 +0000

    Pending changes exported from your codespace

--- commands/play.js
+++ commands/play.js
@@ -55,7 +55,7 @@ module.exports.run = async (client, message, args) => {
 
         if (voiceChannel != null) { 
 
-            message.channel.send(strings.startedPlaying.replace("SONG_TITLE", song.title).replace("url", song.url));
+            message.channel.send(strings.startedPlaying.replace("SONG_TITLE", song.title));
             
             const connection = utils.joinVChannel(voiceChannel);
 
@@ -72,7 +72,7 @@ module.exports.run = async (client, message, args) => {
         serverQueue.songs.push(song);
         utils.log(`Added music to the queue : ${song.title}`)
 
-        return message.channel.send(strings.songAddedToQueue.replace("SONG_TITLE", song.title).replace("url", song.url));
+        return message.channel.send(strings.songAddedToQueue.replace("SONG_TITLE", song.title));
     };
 
 };
